b07839012adc2d93dc214aa06d1c61988b1f4e16
sonota88 <yosiot8753@gmail.com>
2015-05-27 23:14:39 +0900

    get branches

--- git.rb
+++ git.rb
@@ -6,4 +6,32 @@ end
 
 class Git
 
+  def initialize dir
+    @dir = File.expand_path(dir)
+  end
+
+  def read_file path
+    File.read(File.join(@dir, ".git", path))
+  end
+
+  def get_branches
+    Dir.glob(@dir + "/.git/refs/heads/*").map{|path|
+      File.basename(path)
+    }
+  end
+
+  def to_hash
+    {
+      :dir => @dir,
+      :branches => get_branches()
+    }
+  end
+
+end
+
+
+if __FILE__ == $0
+  dir = ARGV[0]
+  git = Git.new(dir)
+  p git.to_hash
 end
