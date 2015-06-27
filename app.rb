86d670cc0a281c708fc66c8e4d557d9ece087941
sonota88 <yosiot8753@gmail.com>
2015-06-27 10:35:29 +0900

    get graph data using API

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "json"
+require "./git"
  
 # sinatraでDNS逆引きを止める | TechRacho
 # http://techracho.bpsinc.jp/baba/2012_11_02/6367
@@ -17,6 +19,45 @@ end
 
 set :bind, '0.0.0.0'
 
+# --------------------------------
+
+def _api(params)
+  api_params = JSON.parse(params[:apiParams])
+  status = "NG"
+  data = {}
+
+  begin
+    data = yield(api_params)
+    status = "OK"
+  rescue => e
+    $stderr.puts e.class, e.message, e.backtrace
+    data = {
+      :msg => "#{e.class}: #{e.message}",
+      :trace => e.backtrace.join("\n")
+    }
+  end
+
+  content_type :json
+  JSON.generate({
+    "status" => status,
+    "data" => data
+  })
+end
+
+# --------------------------------
+
 get "/" do
   send_file "v_root.html"
+end
+
+get "/api/graph" do
+  # for development
+  load "./git.rb"
+
+  _api(params) do |api_params|
+    dir = api_params["dir"]
+    git = Git.new(dir)
+    git.load
+    git.to_hash
+  end
 end
--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,25 @@ function puts(){
   console.log.apply(console, arguments);
 }
 
+function _api(method, url, params, fnOk, fnNg){
+  $.post(url, {
+    _method: method,
+    apiParams: JSON.stringify(params)
+  }, function(data){
+    if(data.status !== "OK"){
+      fnNg(data.data);
+      return;
+    }
+    fnOk(data.data);
+  });
+}
+
 $(function(){
-  puts("ready");
+  _api("get", "/api/graph", {
+    dir: "TODO"
+  }, function(data){
+    puts("OK", data);
+  }, function(data){
+    puts("NG", data);
+  });
 });
